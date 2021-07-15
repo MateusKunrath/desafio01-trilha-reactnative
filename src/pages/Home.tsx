5ed41e52d7eec5177f237dcc65d21faabc2afa46
Mateus Kunrath <mateus_kunrath@hotmail.com>
2021-07-15 16:25:05 -0300

    Desafio 01 da trilha de react native do Ignite concluída

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from '../components/Header'
+import { Task, TasksList } from '../components/TasksList'
+import { TodoInput } from '../components/TodoInput'
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([])
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = [...tasks]
+
+    const taskExists = updatedTasks.find((task) => task.id === id)
+
+    if (taskExists) {
+      taskExists.done = !taskExists.done
+    }
+
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = [...tasks]
+
+    const taskIndex = updatedTasks.findIndex((task) => task.id === id)
+
+    if (taskIndex >= 0) {
+      updatedTasks.splice(taskIndex, 1)
+      setTasks(updatedTasks)
+    }
   }
 
   return (
@@ -26,10 +47,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
@@ -38,6 +59,6 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
+    backgroundColor: '#EBEBEB',
+  },
 })
